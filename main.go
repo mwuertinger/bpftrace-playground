f760478fb780c189acf81978e078531b9c3d7a30
Michael Würtinger <michael.wuertinger@egym.de>
2019-11-15 14:12:41 +0100

    Simple Go tracing example

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"log"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	// start a lot of goroutines calling myfunc indefinitely
+	for i := 0; i < 100; i++ {
+		go func() {
+			buf := make([]byte, 2<<24, 2<<24)
+			for {
+				myfunc(buf)
+			}
+		}()
+	}
+
+	// sleep forever
+	for {
+		time.Sleep(time.Hour)
+	}
+}
+
+// myfunc does some nonsense expensive operations so that we have something
+// to measure with bpftrace.
+func myfunc(buf []byte) {
+	_, err := rand.Read(buf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(sha256.Sum256(buf))
+}
